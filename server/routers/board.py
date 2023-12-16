c4e01ebe489b5d2e821ba7395723701448655ab6
cola <nicolaouhm@gmail.com>
2023-12-16 17:55:31 +0900

    뼈대

---
+++ server/routers/board.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter, Cookie, Header
+from typing import Annotated
+from pydantic import BaseModel
+from typing import Optional
+from ..database.query import execute_sql_query
+from ..controllers.session import getSessionData
+from datetime import datetime
+
+
+router = APIRouter(prefix="/api")
