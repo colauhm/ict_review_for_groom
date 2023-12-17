dd0f9ed2bbd3670d21fd2515476072202c1f325f
cola <nicolaouhm@gmail.com>
2023-12-17 12:40:01 +0900

    회원가입

---
+++ app/utils/function.js
@@ -0,0 +1,70 @@
+export const setCookie = (cookie_name, value, days) => {
+    const exdate = new Date();
+    exdate.setDate(exdate.getDate() + days);
+    // 설정 일수만큼 현재시간에 만료값으로 지정
+
+    var cookie_value = escape(value) + (days == null ? '' : '; expires=' + exdate.toUTCString());
+    document.cookie = cookie_name + '=' + cookie_value;
+};
+
+export const getCookie = (cookie_name) => {
+    var x, y;
+    var val = document.cookie.split(';');
+
+    for (var i = 0; i < val.length; i++) {
+        x = val[i].substr(0, val[i].indexOf('='));
+        y = val[i].substr(val[i].indexOf('=') + 1);
+        x = x.replace(/^\s+|\s+$/g, ''); // 앞과 뒤의 공백 제거하기
+        if (x == cookie_name) {
+            return unescape(y); // unescape로 디코딩 후 값 리턴
+        }
+    }
+};
+
+export const deleteCookie = (cookie_name) => {
+    setCookie(cookie_name, '', -1);
+};
+
+export const serverSessionCheck = async () => {
+    const res = await fetch(getServerUrl() + '/checkSession', { headers: { session: getCookie('session') } });
+    const data = await res.json();
+    return data;
+};
+
+export const authCheck = async () => {
+    const session = getCookie('session');
+    if (session === undefined) {
+        location.href = '/login.html';
+    }
+
+    const data = await serverSessionCheck();
+    // console.log(data);
+    if (!data) {
+        deleteCookie('session');
+        location.href = '/login.html';
+    }
+    return data;
+};
+
+export const authCheckReverse = async () => {
+    const session = getCookie('session');
+    if (session) {
+        const data = await serverSessionCheck();
+        if (data) {
+            location.href = '/';
+        }
+    }
+};
+// 이메일 유효성 검사
+export const validEmail = (email) => {
+    const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    return regExp.test(email);
+};
+
+export const prependChild = (parent, child) => {
+    parent.insertBefore(child, parent.firstChild);
+};
+
+export const ServerUrl = () => {
+    return location.hostname == 'http://uvicornserver/api';
+};
--- server/routers/user.py
+++ server/routers/user.py
@@ -7,6 +7,45 @@ from datetime import datetime
 
 router = APIRouter(prefix="/api")
 
+@router.get('/checkId')
+async def checkEmail(id: str):
+    try:
+        res = await execute_sql_query(
+            "SELECT * FROM user WHERE id = %s", (id,))
+        if len(res) != 0:
+            return 401, "중복된 아이디가 있습니다."
+        else:
+            return 200, {"message": "중복된 아이디가 없습니다."}
+    except Exception as e:
+        return 500, "서버 오류"
+
+# 닉네임 중복 확인
+
+
+@router.get('/checkNickname')
+async def checkNickname(nickname: str):
+    try:
+        res = await execute_sql_query(
+            "SELECT * FROM user WHERE nickname = %s", (nickname,))
+        if len(res) != 0:
+            return 401, "중복된 닉네임이 있습니다."
+        else:
+            return 200, {"message": "중복된 닉네임이 없습니다."}
+    except Exception as e:
+        return 500, "서버 오류"
+    
+@router.get('/checkEmail')
+async def checkEmail(email: str):
+    try:
+        res = await execute_sql_query(
+            "SELECT * FROM user WHERE email = %s", (email,))
+        if len(res) != 0:
+            return 401, "중복된 email이 있습니다."
+        else:
+            return 200, {"message": "중복된 email이 없습니다."}
+    except Exception as e:
+        return 500, "서버 오류"
+    
 class Signup(BaseModel):
     id:str
     password:str
@@ -31,3 +70,12 @@ async def signup(data:Signup):
         print(e)
         return 500, "서버오류" 
     
+@router.get('/checkSession')
+async def checkSession(session: Annotated[str, Header()] = None):
+    try:
+        session_data = await getSessionData(session)
+        if not session_data:
+            return False
+        return session_data
+    except Exception as e:
+        return 500, "서버 오류"
