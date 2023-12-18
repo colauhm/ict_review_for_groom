c7dd35042b211dfa37a560523adfc3528be45520
Cola <nicolaouhm@gmail.com>
2023-12-18 15:51:20 +0900

    실시간 입력값 확인

--- app/signup.js
+++ app/signup.js
@@ -1,17 +1,34 @@
 import {authCheckReverse} from './utils/function.js';
 
+const emailInput = document.getElementById('email');
+const idInput = document.getElementById('id');
+const passwordInput = document.getElementById('password');
+const passwordCheckInput = document.getElementById('passwordCheck');
+const nicknameInput = document.getElementById('nickname');
+
+async function handleInputChange() {
+    const emailValue = emailInput.value;
+    const idValue = idInput.value;
+    const passwordValue = passwordInput.value;
+    const passwordCheckValue = passwordCheckInput.value;
+    const nicknameValue = nicknameInput.value;
+}
+
+function signupInfoCheck() {
+    
+}
+
+// 각 입력 필드에 이벤트 리스너 등록
+emailInput.addEventListener('input', handleInputChange);
+idInput.addEventListener('input', handleInputChange);
+passwordInput.addEventListener('input', handleInputChange);
+passwordCheckInput.addEventListener('input', handleInputChange);
+nicknameInput.addEventListener('input', handleInputChange);
 
 document.getElementById('signupBtn').addEventListener('click', function () {
-    // Form에서 각 필드의 값을 가져오기
-    
-    const email = document.getElementById('email').value;
-    const id = document.getElementById('id').value;
-    const password = document.getElementById('password').value;
-    const passwordCheck = document.getElementById('passwordCheck').value;
-    const nickname = document.getElementById('nickname').value;
 
     // 값이 모두 채워져 있는지 확인
-    if (email && id && password && passwordCheck && nickname) {
+    if (emailValue && idValue && passwordValue && passwordCheckValue && nicknameValue) {
         // 여기에 이메일 인증 등의 추가적인 확인 절차를 넣을 수 있습니다.
         alert('회원가입 성공.');
         // 모든 값이 채워져 있다면 /login.html로 이동
