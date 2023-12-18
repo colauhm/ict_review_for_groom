41d1d6334bce0a48a48eb84b2207979d1db5a6cf
Cola <nicolaouhm@gmail.com>
2023-12-19 00:49:01 +0900

    기본 입력세팅

---
+++ app/boardwrite.js
@@ -0,0 +1,7 @@
+import { authCheck, getCookie, getQueryString, ServerUrl } from './utils/function.js';
+
+const boardComponent = {
+    title : document.getElementById('title'),
+    content : document.getElementById('content'),
+    file: ''
+}
