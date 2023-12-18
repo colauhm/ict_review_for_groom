25213ccb1a89cc5be7005db48559b1e163aadbc2
Cola <nicolaouhm@gmail.com>
2023-12-19 01:06:08 +0900

    보드작성란 기본 세팅

--- app/boardwrite.js
+++ app/boardwrite.js
@@ -3,5 +3,16 @@ import { authCheck, getCookie, getQueryString, ServerUrl } from './utils/functio
 const boardComponent = {
     title : document.getElementById('title'),
     content : document.getElementById('content'),
+    type : '',
     file: ''
 }
+
+const boardCategory = {
+    notice : document.getElementById('notice'),
+    freeBoard : document.getElementById('freeBoard'),
+    QnABoard : document.getElementById('QnABoard'),
+    secretQnABoard : document.getElementById('secretQnABoard')
+}
+
+
+
