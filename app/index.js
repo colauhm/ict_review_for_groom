1a2742d38911568eac6f0b29b9bd21a3760e0ac8
Cola <nicolaouhm@gmail.com>
2023-12-19 00:52:49 +0900

    게시글 작성버튼 -> 게시글 작성란

--- app/index.js
+++ app/index.js
@@ -1,5 +1,8 @@
 import { authCheck, ServerUrl, getCookie } from './utils/function.js';
 
 
+document.getElementById('writePost').addEventListener('click', function (){
+    window.location.href = "/boardwrite.html";
+})
 
 authCheck();
