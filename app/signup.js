44596925ca2bf3b9ab87740740808f5f54b7ac8b
Cola <nicolaouhm@gmail.com>
2023-12-18 21:41:41 +0900

    회원가입 입력 매개변수화

--- app/signup.js
+++ app/signup.js
@@ -5,7 +5,7 @@ const idInput = document.getElementById('id');
 const passwordInput = document.getElementById('pw');
 const passwordCheckInput = document.getElementById('pwck');
 const nicknameInput = document.getElementById('nickname');
-// 논리곱으로 모든값이 true일 때만 true 라고 변하게 사용
+
 const infoCheck = {
     email : false,
     id : false,
@@ -19,20 +19,25 @@ const signupData = {
     nickname: '',
 }
 
+function infoCheckdetect() {
+    return Object.values(infoCheck).every(value => value === true);
+}
+async function capitalizeFirstLetter(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
 function signup(){
     
 }
 
-async function handleCheckInputChange() {
-    const emailValue = emailInput.value;
-    const idValue = idInput.value;
-    const nicknameValue = nicknameInput.value;
-    signupData.email = emailValue;
-    signupData.id = idValue;
-    signupData.nickname = nicknameValue;
-    signupInfoCheck("Email", "email", emailValue);
-    signupInfoCheck("Id", "id", idValue);
-    signupInfoCheck("Nickname", "nickname", nicknameValue);
+async function handleCheckInputChange(param) {
+    const inputValues = {
+        email: emailInput.value,
+        id : idInput.value,
+        nickname : nicknameInput.value
+    }
+    const inputValue = inputValues[param];
+    signupInfoCheck(param, inputValue);
 }
 
 async function handleInputChange(){
@@ -41,14 +46,18 @@ async function handleInputChange(){
     const helperElement = document.querySelector('.inputBox p[name="pwck"]');
     if (passwordCheckValue && passwordValue && passwordCheckValue != passwordValue){
         helperElement.textContent = "일치하지 않습니다.";
-
+        infoCheck.passwordcheck = false;
+    } else if (passwordCheckValue && passwordValue && passwordCheckValue == passwordValue){
+        infoCheck.passwordcheck = true;
+        helperElement.textContent = "";
     } else {
         helperElement.textContent = "";
-
+        infoCheck.passwordcheck = false;
     }
 }
 
-async function signupInfoCheck(Param, param, value){
+async function signupInfoCheck(param, value){
+    const Param = capitalizeFirstLetter(param);
     const helperElement = document.querySelector(`.inputBox p[name="${param}"]`);
     const checkValue = await fetch(ServerUrl() + '/check' + `${Param}` + `?${param}=${value}`, {
         method: 'GET',
@@ -56,35 +65,36 @@ async function signupInfoCheck(Param, param, value){
             'Content-Type': 'application/json'
         }
     });
+    
     const valueck = await checkValue.json();
     //email 값이 존재하면 중복유무 체크 아닐경우 텍스트 없앰
     if (value){
-        
         if (valueck&& valueck[0] == 200){
             helperElement.textContent = `사용가능한 ${param}입니다.`
-       
+            infoCheck[param] = true;
+            signupData[param] = value;
         } else {
             helperElement.textContent = `이미 존재하는 ${param}입니다.`
-           
+            infoCheck[param] = false;
         }
     } else{
-    
+        infoCheck[param] = false;
     }
 }
 
 
 // 각 입력 필드에 이벤트 리스너 등록
-emailInput.addEventListener('input', handleCheckInputChange);
-idInput.addEventListener('input', handleCheckInputChange);
+emailInput.addEventListener('input', handleCheckInputChange("Email", "email"));
+idInput.addEventListener('input', handleCheckInputChange("Id", "id"));
 passwordInput.addEventListener('input', handleInputChange);
 passwordCheckInput.addEventListener('input', handleInputChange);
-nicknameInput.addEventListener('input', handleCheckInputChange);
+nicknameInput.addEventListener('input', handleCheckInputChange("Nickname", "nickname"));
 
 document.getElementById('signupBtn').addEventListener('click', function () {
-
+    console.log(infoCheck);
     // 값이 모두 채워져 있는지 확인
-    if (emailInput && idInput && passwordInput && passwordCheckInput && nicknameInput) {
-        if (infoCheck){
+    if (emailInput.value && idInput.value && passwordInput.value && passwordCheckInput.value && nicknameInput.value) {
+        if (infoCheckdetect){
             alert('회원가입 성공 로그인 페이지로 이동합니다.');
             // 모든 값이 채워져 있다면 /login.html로 이동
             window.location.href = '/login.html';
@@ -92,8 +102,6 @@ document.getElementById('signupBtn').addEventListener('click', function () {
             alert('입력하신 정보를 다시 확인해 주세요.');
         }
         // 여기에 이메일 인증 등의 추가적인 확인 절차를 넣을 수 있습니다.
-        
-        
     } else {
         // 모든 값이 채워져 있지 않다면 사용자에게 알림 등을 보여줄 수 있습니다.
         alert('모든 칸을 채워주세요.');
