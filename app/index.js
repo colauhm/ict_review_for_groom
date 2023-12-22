33fb26339b072ad3c80a5e8c070a36033d59795d
Cola <nicolaouhm@gmail.com>
2023-12-22 17:00:04 +0900

    비밀글 체크기능 추가

--- app/index.js
+++ app/index.js
@@ -38,6 +38,8 @@ const searchDetailTypebutton = {
 
 const selectBoardButtons = document.querySelector('.selectBoardButtons');
 const secretQnABoardSelector = document.querySelector('.secretQnABoardSelector');
+const secretTypeButton = document.getElementById('secretTypeButton');
+const secretCheckBox = document.getElementById('secretCheckBox');
 
 //--------------------버튼 선택시 다른 버튼은 선택버튼 활성화 나머지버튼 활성화 기능-----------//
 
@@ -48,6 +50,8 @@ function setupButtons(typeButton, listType, methodKey) {
             listType[methodKey] = selectedButtonName;
             const newBoardList = await boardListLoad();
             setBoardItem(newBoardList);
+            console.log(requestBoardListType);
+            console.log(searchBoardListType);
         });
     });
 }
@@ -62,39 +66,22 @@ async function typeChoice(clickedButton, allButtons) {
 
 //-------------------------------게시판 선택 버튼 및 기능-----------------------------------//
 
-
 document.getElementById('writePost').addEventListener('click', function (){
     window.location.href = "/boardwrite.html";
 })
 
 boardCategory.noticeSelector.disabled = true;
+secretTypeButton.disabled = true;
 
 setupButtons(boardCategory, requestBoardListType, 'category');
 
-// Object.values(boardCategory).forEach(async clickElement => {
-//     clickElement.addEventListener('click',BoardTypeChoice);
-//     const newBoardList = await boardListLoad();
-//     setBoardItem(newBoardList);
-// });
-
-selectBoardButtons.addEventListener('change', async () => {
-    QnAcheck(requestBoardListType.category)
+selectBoardButtons.addEventListener('click', async () => {
+    QnAcheck(requestBoardListType.category);
     await authCheck();
 });
 
-
-// secretQnABoardSelector.addEventListener('change', function(){
-//     requestBoardListType.category = requestBoardListType.category == 'QnA'? 'secretQnA':'QnA';
-//     if (requestBoardListType.category == 'secretQnA'){
-//         sortTypebutton.viewSorter.style.display = 'none';
-//     } else{
-//         sortTypebutton.viewSorter.style.display = 'block';
-//     }
-//     //console.log(requestBoardListType)
-// })
-
 function QnAcheck(boardType){
-    if (boardType == 'QnA'){
+    if (boardType == 'QnA' || boardType == 'secretQnA'){
         secretQnABoardSelector.style.display = 'block';
         sortTypebutton.recommendSorter.style.display = 'none';
         secretQnAcheck(boardType);
@@ -102,35 +89,19 @@ function QnAcheck(boardType){
     } else {
         secretQnABoardSelector.style.display = 'none';
         sortTypebutton.recommendSorter.style.display = 'block';
+        
     }
+    
 }
 
 function secretQnAcheck(boardType){
-    requestBoardListType.category = boardType == 'QnA'? 'secretQnA':'QnA';
-    if (requestBoardListType.category == 'secretQnA'){
+    requestBoardListType.category = secretCheckBox.checked ? 'QnA':'secretQnA';
+    if (boardType == 'secretQnA'){
         sortTypebutton.viewSorter.style.display = 'none';
-    } else{
+    } else {
         sortTypebutton.viewSorter.style.display = 'block';
     }
 }
-// async function BoardTypeChoice(){
-//     authCheck();
-//     const boardTypebuttonId = this.id;
-//     requestBoardListType.category = this.name;
-//     if (boardTypebuttonId == "QnABoardSelector"){
-//         secretQnABoardSelector.style.display = 'block';
-//         sortTypebutton.recommendSorter.style.display = 'none';
-//     } else {
-//         secretQnABoardSelector.style.display = 'none';
-//         sortTypebutton.recommendSorter.style.display = 'block';
-//     }
-//     Object.values(boardCategory).forEach(button => {button.disabled = false;});
-//     boardCategory[boardTypebuttonId].disabled = true;
-//     //console.log(requestBoardListType)
-//     //console.log(boardList);
-//     const newBoardList = await boardListLoad();
-//     setBoardItem(newBoardList);
-// }
 
 //---------------------------------------정렬 선택 부분----------------------------------------//
 
@@ -139,68 +110,21 @@ sortTypebutton.recentSorter.disabled = true;
 
 setupButtons(sortTypebutton, requestBoardListType, 'sortMethod');
 
-
-
-// Object.values(sortTypebutton).forEach(button => {
-//     button.addEventListener('click', sortTypeChoice);
-// });
-
-// async function sortTypeChoice() {
-//     const sortTypebuttonId = this.id;
-//     requestBoardListType.sortMethod = this.name
-//     Object.values(sortTypebutton).forEach(button => {
-//         button.disabled = false;
-//     });
-//     sortTypebutton[sortTypebuttonId].disabled = true;
-//     //console.log(requestBoardListType);
-//     const newBoardList = await boardListLoad();
-//     setBoardItem(newBoardList);
-// }
-
 //-------------------------------------검색 기준 선택 부분-------------------------------------//
 
 searchTypeButton.all.disabled = true;
 
 setupButtons(searchTypeButton, searchBoardListType, 'category');
 
-// Object.values(searchTypeButton).forEach(button => {
-//     button.addEventListener('click', searchTypeChoice);
-// });
-
-// function searchTypeChoice() {
-//     const searchTypebuttonname = this.name;
-//     searchBoardListType.category = searchTypebuttonname
-//     Object.values(searchTypeButton).forEach(button => {
-//         button.disabled = false;
-//     });
-//     searchTypeButton[searchTypebuttonname].disabled = true;
-//     //console.log(searchBoardListType);
-// }
-
-
 searchDetailTypebutton.title.disabled = true;
 
 setupButtons(searchDetailTypebutton, searchBoardListType, 'datailCategory');
 
-// Object.values(searchDetailTypebutton).forEach(button => {
-//     button.addEventListener('click', searchDetailTypeChoice);
-// });
-
-// function searchDetailTypeChoice(){
-//     const searchDetailTypebuttonName = this.name;
-//     searchBoardListType.datailCategory = searchDetailTypebuttonName
-//     Object.values(searchDetailTypebutton).forEach(button => {
-//         button.disabled = false;
-//     });
-//     searchDetailTypebutton[searchDetailTypebuttonName].disabled = true;
-//     //console.log(searchBoardListType);
-// }
 //--------------------------------------보드 요소 불러오기---------------------------------------//
-
 
 async function boardListLoad(){
     const {category, sortMethod} = requestBoardListType;
-    console.log(requestBoardListType, searchBoardListType);
+    //console.log(requestBoardListType, searchBoardListType);
     const boardList = await fetch(ServerUrl() + '/boards' + `?category=${category}` + `&sortType=${sortMethod}`, {noCORS: true });
     const data = await boardList.json();
     //console.log(data);
@@ -219,8 +143,6 @@ const setBoardItem = async (boardData) => {
 };
 
 //--------------------------------세션 유무에 따라 보이는 버튼 다르게-----------------------------//
-
-
 
 const req = await fetch(ServerUrl() + '/checkSession', { headers: { session: getCookie('session') } });
 const myInfo = await req.json();
